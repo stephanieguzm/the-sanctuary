62c9d1fa613fed5a476cfdd5a27e9257343b028a
Stephanie Guzman <sguzman247@gmail.com>
2022-08-03 12:15:55 -0500

    Add sample data to files

---
+++ src/data/bookings-sample-data.js
@@ -0,0 +1,62 @@
+const bookings = [
+  {
+    id: "5fwrgu4i7k55hl6t8",
+    userID: 1,
+    date: "2022/02/05",
+    roomNumber: 12
+  },
+  {
+    id: "5fwrgu4i7k55hl6x8",
+    userID: 1,
+    date: "2023/01/11",
+    roomNumber: 20
+  },
+  {
+    id: "5fwrgu4i7k55hl727",
+    userID: 1,
+    date: "2022/11/06",
+    roomNumber: 22
+  },
+  {
+    id: "5fwrgu4i7k55hl72h",
+    userID: 1,
+    date: "2023/12/22",
+    roomNumber: 15
+  }, 
+  {
+    id: "5fwrgu4i7k55hl72q",
+    userID: 1,
+    date: "2022/01/19",
+    roomNumber: 19
+  },
+  {
+    id: "5fwrgu4i7k55hl74i",
+    userID: 3,
+    date: "2023/01/13",
+    roomNumber: 15
+  },
+  {
+    id: "5fwrgu4i7k55hl76o",
+    userID: 3,
+    date: "2023/02/20",
+    roomNumber: 14
+  },
+  {
+    id: "5fwrgu4i7k55hl7a6",
+    userID: 3,
+    date: "2022/02/02",
+    roomNumber: 16
+  },
+  {
+    id: "5fwrgu4i7k55hl7aa",
+    userID: 3,
+    date: "2023/02/14",
+    roomNumber: 20
+  },
+  {
+    id: "5fwrgu4i7k55hl7d9",
+    userID: 3,
+    date: "2022/02/15",
+    roomNumber: 2
+  },
+];
---
+++ src/data/rooms-sample-data.js
@@ -0,0 +1,98 @@
+const rooms = [
+  {
+    number: 10,
+    roomType: "suite",
+    bidet: false,
+    bedSize: "twin",
+    numBeds: 1,
+    costPerNight: 497.64
+  },
+  {
+    number: 11,
+    roomType: "single room",
+    bidet: true,
+    bedSize: "twin",
+    numBeds: 2,
+    costPerNight: 207.24
+  },
+  {
+    number: 12,
+    roomType: "single room",
+    bidet: false,
+    bedSize: "twin",
+    numBeds: 2,
+    costPerNight: 172.09
+  },
+  {
+    number: 15,
+    roomType: "residential suite",
+    bidet: false,
+    bedSize: "full",
+    numBeds: 1,
+    costPerNight: 294.56
+  },
+  {
+    number: 16,
+    roomType: "single room",
+    bidet: false,
+    bedSize: "full",
+    numBeds: 2,
+    costPerNight: 325.6
+  },
+  {
+    number: 2,
+    roomType: "suite",
+    bidet: false,
+    bedSize: "full",
+    numBeds: 2,
+    costPerNight: 477.38
+  },
+  {
+    number: 4,
+    roomType: "single room",
+    bidet: false,
+    bedSize: "queen",
+    numBeds: 1,
+    costPerNight: 429.44
+  },
+  {
+    number: 5,
+    roomType: "single room",
+    bidet: true,
+    bedSize: "queen",
+    numBeds: 2,
+    costPerNight: 340.17
+  },
+  {
+    number: 6,
+    roomType: "junior suite",
+    bidet: true,
+    bedSize: "queen",
+    numBeds: 1,
+    costPerNight: 397.02
+  },
+  {
+    number: 8,
+    roomType: "junior suite",
+    bidet: false,
+    bedSize: "king",
+    numBeds: 1,
+    costPerNight: 261.26
+  },
+  {
+    number: 18,
+    roomType: "junior suite",
+    bidet: false,
+    bedSize: "king",
+    numBeds: 2,
+    costPerNight: 496.41
+  },
+  {
+    number: 3,
+    roomType: "single room",
+    bidet: false,
+    bedSize: "king",
+    numBeds: 1,
+    costPerNight: 491.14
+  }
+]
