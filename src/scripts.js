5c27c0faecf9077f4896ce5c15db5468098080b0
Stephanie Guzman <sguzman247@gmail.com>
2022-08-07 14:46:33 -0500

    Add function to capture check-in date

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,32 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/logo.png'
+import './images/courtyard.png'
+import './images/residential-suite.png'
+import './images/suite.png'
+import './images/junior-suite.png'
+import './images/single.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import Booking from '../src/classes/Booking';
+import Hotel from '../src/classes/Hotel'; 
+import Customer from '../src/classes/Customer';
+import Room from '../src/classes/Room';
+
+const reservationsButton = document.querySelector('#dashboardButton');
+const checkInDateInput = document.querySelector('#check-in-date');
+
+checkInDateInput.addEventListener('change', (event) => {
+  checkDate(event)
+})
+
+let checkInDate;
+
+const checkDate = (event) => {
+  checkInDate = event.target.value.split('-').join('/');
+  console.log(checkInDate)
+  return checkInDate
+}
+
