ab827d694ba9c7f879da356abacd522f5e827853
Stephanie Guzman <sguzman247@gmail.com>
2022-08-08 16:52:53 -0500

    Add api helper functions

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,24 +1,25 @@
 var getData = (url) => {
   return fetch(url)
+    .then(checkStatus)
     .then(response => response.json())
-    .catch(error => console.log('Looks like there was a problem!', error))
+    .catch(error => generateErrorMessage(error))
 }
 
-// var checkStatus = (response) => {
-//   if (response.ok) {
-//     return Promise.resolve(response);
-//   } else {
-//     return Promise.reject(new Error(generateErrorMessage(response)));
-//   }
-// }
+var checkStatus = (response) => {
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(generateErrorMessage(response)));
+  }
+}
 
-// var generateErrorMessage = (response) => {
-//   errorMessageContent.innerHTML = '';
+var generateErrorMessage = (response) => {
+  console.log('Error: ', response)
+  // errorMessageContent.innerHTML = '';
 
-//   errorMessageContent.innerHTML +=
-//     `<h2 class="modal-title" id="error-message-modal">${response.statusText}: Oops! Looks like there was an error!</h2>`;
-//   MicroModal.show("modal-3")
-// }
+  // errorMessageContent.innerHTML +=
+  //   `<h2 class="modal-title" id="error-message-modal">${response.statusText}: Oops! Looks like there was an error!</h2>`;
+  // MicroModal.show("modal-3")
+}
 
-export default getData;
-// export default { getData, checkStatus };
+export { getData, checkStatus, generateErrorMessage };
